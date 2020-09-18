763de9f3ef86cc8d2f86fc26a970ebf25f1074a0
Nishit <nishitdjoshi@gmail.com>
2020-09-17 21:15:14 -0500

    Weather card

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 window.addEventListener("load",()=> {
-    let lat;
-    let long;
+
     let timeZone = document.querySelector(".location-timezone");
     let temperature = document.querySelector(".temperature-degree");
     let icon = document.querySelector(".icon");
     let temperatureDescription = document.querySelector(".temperature-description");
+    let wind = document.querySelector(".windspeed");
 
-    // if(navigator.geolocation){
-    //     navigator.geolocation.getCurrentPosition(position => {
-    //         long = position.coords.longitude;
-    //         lat = position.coords.latitude;
-         const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
          const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=41.257160&lon=-95.995102&units=imperial&lang=en&appid=23d42c6f1d8fba0e824c8e1e2e4590dd`;
 
             fetch(api)
@@ -19,15 +15,15 @@ window.addEventListener("load",()=> {
                 }).then(data => {
                     const locationTimeZone = data.name;
                     const desc = data.weather[0].description;
-                    const weatherIcon = data.weather[0].icon;
-                    const temp = data.main.temp;
+                   // const weatherIcon = data.weather[0].icon;
+                    const temp = Math.round(data.main.temp);
+                    const windSp = Math.round(data.wind.speed);
 
-                icon.innerHTML = weatherIcon;
+               // icon.innerHTML = weatherIcon;
                 temperature.innerHTML = temp;
+                wind.innerHTML = windSp;
                 temperatureDescription.innerHTML = desc;
                 timeZone.innerHTML = locationTimeZone;
                 });
-       // });
 
-   // }
 });
